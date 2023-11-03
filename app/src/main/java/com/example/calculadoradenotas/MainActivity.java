3c0dea2b2258afbc220ca9bb889542234b39f131
saviocunhaa <saviocunha.01@hotmail.com>
2023-11-03 07:35:16 -0300

    Meu primeiro app android

---
+++ app/src/main/java/com/example/calculadoradenotas/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.calculadoradenotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //criando objetos para o id
+    private EditText n1,n2,n3,n4, numeroFaltas;
+    private Button btCalcular;
+    TextView txtResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //recuperando todos os IDs da Activity main
+        n1 = findViewById(R.id.nota1);
+        n2 = findViewById(R.id.nota2);
+        n3 = findViewById(R.id.nota3);
+        n4 = findViewById(R.id.nota4);
+        numeroFaltas = findViewById(R.id.numeroFalta);
+        btCalcular = findViewById(R.id.btCalcular);
+        txtResultado = findViewById(R.id.txtResultado);
+
+        //instaciar botão calcular para ativar o clik
+        btCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log.i("bt", "Botão ok"); // Esse log serve para testar o eveto de clique que será visto no Logcat
+
+
+                double mediaFinal;
+                //receber todos os dados em estringe e converter para seus tipos especificos
+                double nota1 = Double.parseDouble(n1.getText().toString());
+                double nota2 = Double.parseDouble(n2.getText().toString());
+                double nota3 = Double.parseDouble(n3.getText().toString());
+                double nota4 = Double.parseDouble(n4.getText().toString());
+                int numeroFalta = Integer.parseInt(numeroFaltas.getText().toString());
+
+                mediaFinal = (nota1 + nota2 + nota3 + nota4)/4;
+                //iniciar verificação de clausulas
+                if (mediaFinal >= 7 && numeroFalta <= 20){
+                    txtResultado.setText("Aluno Aprovado \n Média: " + mediaFinal);
+                    txtResultado.setTextColor(getColor(R.color.green));
+                }else if(numeroFalta > 20) {
+                    txtResultado.setText("Aluno Reprovado por Falta \n Faltas: "+ numeroFalta);
+                    txtResultado.setTextColor(getColor(R.color.red));
+                } else {
+                    txtResultado.setText("Aluno Reprovado \n Media: " + mediaFinal);
+                    txtResultado.setTextColor(getColor(R.color.red));
+                }
+
+
+
+            }
+        });
+
+    }
+}
